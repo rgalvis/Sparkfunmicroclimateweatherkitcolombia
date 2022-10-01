7228894feba4669fa76346de43ae510321c56fcc
Reynel <114690506+rgalvis@users.noreply.github.com>
2022-10-01 12:00:43 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+let trama = ""
+let MIN_ANTERIOR = 0
+let MIN_ACTUAL = 0
+weatherbit.startWeatherMonitoring()
+weatherbit.startRainMonitoring()
+weatherbit.startWindMonitoring()
+serial.redirect(
+SerialPin.P15,
+SerialPin.P14,
+BaudRate.BaudRate9600
+)
+serial.writeLine("FECHA" + ";" + "HORA" + ";" + "TEMP (°C)" + ";" + "PRESION (msnm)" + ";" + "PRECI(mm)" + ";" + "VIENT(DIR)" + ";" + "VEL/VIENTO(Km/h)" + ";" + "ALT(M)")
+basic.forever(function () {
+    basic.pause(60000)
+    MIN_ACTUAL = DS1307.getMinute()
+    if (MIN_ACTUAL % 5 == 0 && MIN_ACTUAL != MIN_ANTERIOR) {
+        basic.showIcon(IconNames.Ghost)
+        MIN_ANTERIOR = MIN_ACTUAL
+        trama = "" + DS1307.getDay() + "/" + DS1307.getMonth() + "/" + DS1307.getYear() + ";" + DS1307.getHour() + ":" + DS1307.getMinute() + ";" + (weatherbit.temperature() + 100) + ";" + (weatherbit.pressure() + 25600) + ";" + (weatherbit.rain() + 25.4) + ";" + weatherbit.windDirection() + ";" + weatherbit.windSpeed() * 1.6 + ";" + weatherbit.altitude()
+        basic.pause(2000)
+        serial.writeLine(trama)
+        basic.clearScreen()
+    }
+})
