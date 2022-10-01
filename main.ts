67f8b06f3995b0072547ece00b5a14b5bdb7b0e9
Reynel <114690506+rgalvis@users.noreply.github.com>
2022-10-01 12:16:40 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 let trama = ""
-let MIN_ANTERIOR = 0
 let MIN_ACTUAL = 0
 weatherbit.startWeatherMonitoring()
 weatherbit.startRainMonitoring()
@@ -10,10 +9,11 @@ SerialPin.P14,
 BaudRate.BaudRate9600
 )
 serial.writeLine("FECHA" + ";" + "HORA" + ";" + "TEMP (°C)" + ";" + "PRESION (msnm)" + ";" + "PRECI(mm)" + ";" + "VIENT(DIR)" + ";" + "VEL/VIENTO(Km/h)" + ";" + "ALT(M)")
+let MIN_ANTERIOR = DS1307.getMinute()
 basic.forever(function () {
     basic.pause(60000)
     MIN_ACTUAL = DS1307.getMinute()
-    if (MIN_ACTUAL % 5 == 0 && MIN_ACTUAL != MIN_ANTERIOR) {
+    if (MIN_ACTUAL % 1 == 0 && MIN_ACTUAL != MIN_ANTERIOR) {
         basic.showIcon(IconNames.Ghost)
         MIN_ANTERIOR = MIN_ACTUAL
         trama = "" + DS1307.getDay() + "/" + DS1307.getMonth() + "/" + DS1307.getYear() + ";" + DS1307.getHour() + ":" + DS1307.getMinute() + ";" + (weatherbit.temperature() + 100) + ";" + (weatherbit.pressure() + 25600) + ";" + (weatherbit.rain() + 25.4) + ";" + weatherbit.windDirection() + ";" + weatherbit.windSpeed() * 1.6 + ";" + weatherbit.altitude()
